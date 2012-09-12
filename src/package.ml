f9dbec409965e689259ef3c86df0783ea4d9e1c8
Guillem Rieu <guillem.rieu@ocamlpro.com>
2012-09-12 14:05:17 +0200

    [add] Adding maintainer and source to package info

--- src/package.ml
+++ src/package.ml
@@ -7,7 +7,54 @@ let to_html (repository: Path.R.t) (pkg: Types.NV.t): Cow.Html.t =
   let pkg_descr =
     File.Descr.full (File.Descr.read (Path.R.descr repository pkg))
   in
+  let pkg_url =
+    try
+      let url_file = File.URL.read (Path.R.url repository pkg) in
+      let kind = match File.URL.kind url_file with
+        | Some k -> <:xml< [$str: k$] >>
+        | None -> <:xml< >>
+      in
+      let checksum = match File.URL.checksum url_file with
+        | Some c -> <:xml< <small>$str: c$</small> >>
+        | None -> <:xml< >>
+      in
+      let url = File.URL.url url_file in
+      <:xml<
+        <tr>
+          <th>Source $kind$</th>
+          <td>
+            <a href="$str: url$" title="Download source">$str: url$</a><br />
+            $checksum$
+          </td>
+        </tr>
+      >>
+    with
+      Globals.Exit 66 -> <:xml< >>
+  in
+  let opam_file = File.OPAM.read (Path.R.opam repository pkg) in
+  let pkg_maintainer = File.OPAM.maintainer opam_file in
   <:xml<
     <h2>$str: pkg_name$ $str: pkg_version$</h2>
+
+    <table class="table">
+      <tbody>
+        <tr>
+          <th>Maintainer</th>
+          <td>
+            $str: pkg_maintainer$
+          </td>
+        </tr>
+        $pkg_url$
+        <tr>
+          <th>Dependencies</th>
+          <td></td>
+        </tr>
+        <tr>
+          <th>Optional dependencies</th>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+
     <p>$str: pkg_descr$</p>
   >>
