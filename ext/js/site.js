c1ae6e615f0c3099fe9575b34740f9acd3b244dc
Guillem Rieu <guillem.rieu@ocamlpro.com>
2012-09-19 18:45:04 +0200

    [fix] Disabling Enter key in search box (to avoid reloading page since search is done as you type)

--- ext/js/site.js
+++ ext/js/site.js
@@ -1,11 +1,16 @@
 !function ($) {
   $(function(){
-    window.prettyPrint && prettyPrint()
+    window.prettyPrint && prettyPrint();
 
     // TODO: enable tooltips
     $('.tooltip-packages').tooltip({
       selector: "a[rel=tooltip]"
-    })
+    });
+
+    // Disable Enter in the search form (live JS search instead)
+    $('.form-search').submit(function(e) {
+      e.preventDefault();
+    });
 
   })
 }(window.jQuery)
