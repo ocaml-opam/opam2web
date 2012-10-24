cc388966e77ecfabda393893927dd6c7e06a10e8
Guillem Rieu <guillem.rieu@ocamlpro.com>
2012-10-24 17:10:53 +0200

    [fix] Fixing monthes index for timestamp creation

--- src/statistics.ml
+++ src/statistics.ml
@@ -4,18 +4,18 @@ open Logentry
 open O2w_common
 
 let month_of_string: string -> int = function
-  | "Jan" -> 1
-  | "Feb" -> 2
-  | "Mar" -> 3
-  | "Apr" -> 4
-  | "May" -> 5
-  | "Jun" -> 6
-  | "Jul" -> 7
-  | "Aug" -> 8
-  | "Sep" -> 9
-  | "Oct" -> 10
-  | "Nov" -> 11
-  | "Dec" -> 12
+  | "Jan" -> 0
+  | "Feb" -> 1
+  | "Mar" -> 2
+  | "Apr" -> 3
+  | "May" -> 4
+  | "Jun" -> 5
+  | "Jul" -> 6
+  | "Aug" -> 7
+  | "Sep" -> 8
+  | "Oct" -> 9
+  | "Nov" -> 10
+  | "Dec" -> 11
   | unknown -> failwith ("Unknown month: " ^ unknown)
 
 (* Retrieve log entries of an apache access.log file *)
