8ff0736170fc8ac639612d13209a4ee8a804f4be
Raja Boujbel <raja.boujbel@ocamlpro.com>
2018-05-17 15:32:18 +0200

    cleaning

--- src/o2wHome.ml
+++ src/o2wHome.ml
@@ -74,7 +74,6 @@ let to_html ~content_dir ~statistics ~news univ =
       let popularity_fn pkg =
         try OpamPackage.Name.Map.find pkg.name sset
         with Not_found -> 0L in
-      let packages = univ.st.packages in
       let nb_packages = OpamPackage.Set.cardinal latest_packages in
       let top10_pkgs = O2wStatistics.top_packages ~ntop: 10 popularity_fn latest_packages in
       let top10_items = List.map mk_top_li top10_pkgs in
