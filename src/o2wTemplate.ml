7002174672c70e82730f1c381aa9b5c6e584cf5d
Kate <kit.ty.kate@disroot.org>
2021-11-15 18:49:27 +0000

    Remove google analytics integration

--- src/o2wTemplate.ml
+++ src/o2wTemplate.ml
@@ -53,20 +53,7 @@ let create ~title ~header ~body ~footer ~depth =
     ]
   in
   let js_html =
-    Html.script ~ty:"text/javascript"
-      (Html.string
-         "var _gaq = _gaq || []; \
-          _gaq.push(['_setAccount', 'UA-22552764-5']); \
-          _gaq.push(['_trackPageview']); \
-          (function() { \
-          var ga = document.createElement('script'); \
-          ga.type = 'text/javascript'; ga.async = true; \
-          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : \
-          'http://www') + '.google-analytics.com/ga.js'; \
-          var s = document.getElementsByTagName('script')[0]; \
-          s.parentNode.insertBefore(ga, s);
-          })();")
-    @ List.flatten
+    List.flatten
       (List.rev_map (fun f ->
            Html.script
              ~src:(prepend_root (Uri.make ~path:f ()))
